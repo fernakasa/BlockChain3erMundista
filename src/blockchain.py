88f35279c78bc7af9402a38444fd899a4febf289
fernakasa <fernakasa@gmail.com>
2021-04-07 19:38:54 -0300

    Update Chain, Block and Genesis
    Se agrega:
    Class Blockchain
    + List Cadena
    + Genesis Generator
    
    Update Block
    + Atributos para Hashes

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,8 +1,32 @@
 from datetime import datetime
+from hashlib import sha256
+import json
 
 class Block:
-    def __init__(self, cor, mot, arc):
+    def __init__(self, cor, mot, arc, hant, hblock, hverif):
         self.correo = cor
         self.motivo = mot
         self.archivo = arc
-        self.timestamp = datetime.now()
+        #self.timestamp = datetime.now()
+        self.hashAnt = hant
+        self.hashBlock = hblock
+        self.hashVerif = hverif
+
+    def crearHash(self):
+        hash = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(hash.encode()).hexdigest()
+
+    def mostrar(self):
+        print("A nombre de: ", self.correo)
+        print("Motivo: ", self.motivo)
+        #print("Fecha: ", self.tiempo)
+
+class Blockchain:
+    def __init__(self):
+        self.cadena = []
+        self.crearGenesis()
+
+    def crearGenesis(self):
+        bloqueGenesis = Block("", "", "", "0", "0", "0")
+        bloqueGenesis.hashBlq = bloqueGenesis.crearHash()
+        self.cadena.append(bloqueGenesis) 
--- test/blockchaintest.py
+++ test/blockchaintest.py
@@ -6,12 +6,12 @@ import unittest
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
 
-from src.blockchain import Block
+from src.blockchain import Block, Blockchain
 
 class Test(unittest.TestCase):
     def test_block(self):
-        test = Block("test@test.com",1,"a")
-        self.assertEqual(test.motivo, 1)
+        test = Blockchain()
+        self.assertEqual(test.cadena[0].correo, "")
         
 if __name__ == '__main__':
     unittest.main()

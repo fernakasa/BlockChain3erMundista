6fe9aa1751c2604390794b5c8c4d2eaeb8f092a6
fernakasa <fernakasa@gmail.com>
2021-04-11 21:58:45 -0300

    Update #001
    Update:
    +Bloque.py
    + Blockchain
    ++ crearBloque
    ++ getNextBloqueIndex
    ++ getPreviusBloqueHash
    
    +blockChainTest
    ++test_is_bloque_should_to_be_true_when_bloque_is_created
    ++test_is_bloque_genesis_hash_anterior_should_to_be_true_when_hash_anterior_is_string_zero
    ++test_is_bloque_One_should_to_be_true_when_bloque_One_is_created

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,20 +1,5 @@
+from src.bloque import Bloque
 from datetime import datetime
-from hashlib import sha256
-import json
-
-class Bloque:
-    def __init__(self, index, cor, mot, hashArc, hashAnt):
-        self.index = index
-        self.correo = cor
-        self.motivo = mot
-        self.hashArc = hashArc
-        self.hashAnt = hashAnt
-        self.hashBloque = self.crearHash()
-        #self.timestamp = datetime.now()
-
-    def crearHash(self):
-        hash = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(hash.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
@@ -22,11 +7,25 @@ class Blockchain:
         self.crearGenesis()
 
     def crearGenesis(self):
-        bloqueGenesis = Bloque(0, "", "", "0", "0")
+        bloqueGenesis = Bloque(0, "", "", "0", "0", "2021-01-01 00:00:00")
         self.cadena.append(bloqueGenesis)
     
-    def crearBloque(self):
-        pass
+    def crearBloque(self, cor, mot, hashArc):
+        newBloque = Bloque(self.getNextBloqueIndex(), cor, mot, hashArc, self.getPreviusBloqueHash(), "2021-01-01 22:00:00")
+        self.cadena.append(newBloque)
+
+    def getNextBloqueIndex(self):
+        return len(self.cadena)
+
+    def getPreviusBloqueHash(self):
+        return self.getHashByIndex(self.getNextBloqueIndex() - 1)
+
+    def getDateTimeString(self): 
+        datatime = datetime.now()
+        return datatime.isoformat()
+
+    def getFormatDate(self, timestamp):
+        return datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
 
     def getHashByIndex(self, index):
         return self.cadena[index].hashBloque
--- test/blockchaintest.py
+++ test/blockchaintest.py
@@ -1,37 +1,48 @@
 import os
 import sys
+from datetime import datetime
 
 import unittest
 
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
 
-from src.blockchain import Bloque, Blockchain
+from src.blockchain import Blockchain
+from src.bloque import Bloque
+
+test = Blockchain()
 
 class Test(unittest.TestCase):
-    def test_is_bloque_genesis_correo_should_to_be_true_when_correo_is_empty(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].correo, "")
+    def test_is_bloque_should_to_be_true_when_bloque_is_created(self):
+        test = Bloque(0, 'correo@prueba.com', 'prueba', 'hash_archivo', 'hash_anterior', '2021-04-11 21:00:00')
+        test.setDateTimeString = '10'
+        self.assertEqual(0, test.index)
+        self.assertEqual("correo@prueba.com", test.correo)
+        self.assertEqual("prueba", test.motivo)
+        self.assertEqual("hash_archivo", test.hashArc)
+        self.assertEqual("hash_anterior", test.hashAnt)
+        self.assertEqual("2021-04-11 21:00:00", test.timestamp)
+        self.assertEqual('a40bfe4dd0bf234716b76a3d5e75a40286e36b6849ccac735720151dc49c2662', test.hashBloque)
+    
+    def test_is_bloque_genesis_should_to_be_true_when_bloque_genesis_is_created(self):
+        #test = Blockchain()
+        self.assertEqual(0, test.cadena[0].index)
+        self.assertEqual("", test.cadena[0].correo)
+        self.assertEqual("", test.cadena[0].motivo)
+        self.assertEqual("0", test.cadena[0].hashArc)
+        self.assertEqual("0", test.cadena[0].hashAnt)
+        self.assertEqual("2021-01-01 00:00:00", test.cadena[0].timestamp)
+        self.assertEqual('efa3939f1f6768c242cb0e68ba9ad6cc10f10f7624f30a5cce51f640068b73c5', test.getHashByIndex(0))
+    
+    def test_is_bloque_One_should_to_be_true_when_bloque_One_is_created(self):
+        test.crearBloque("correo@bloqueOne.com", "prueba", "hashArc")
+        self.assertEqual(1, test.cadena[1].index)
+        self.assertEqual("correo@bloqueOne.com", test.cadena[1].correo)
+        self.assertEqual("prueba", test.cadena[1].motivo)
+        self.assertEqual("hashArc", test.cadena[1].hashArc)
+        self.assertEqual("efa3939f1f6768c242cb0e68ba9ad6cc10f10f7624f30a5cce51f640068b73c5", test.cadena[1].hashAnt)
+        self.assertEqual("2021-01-01 22:00:00", test.cadena[1].timestamp)
+        self.assertEqual('001dafe88b0d14e0cc71ce06947dd750f74a46c74d0fc0879484fa42a439b048', test.getHashByIndex(1))
 
-    def test_is_bloque_genesis_index_should_to_be_true_when_index_is_zero(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].index, 0)
-    
-    def test_is_bloque_genesis_motivo_should_to_be_true_when_motivo_is_empty(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].motivo, "")
-    
-    def test_is_bloque_genesis_hash_archivo_should_to_be_true_when_hash_archivo_is_string_zero(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].hashArc, "0")
-    
-    def test_is_bloque_genesis_hash_anterior_should_to_be_true_when_hash_anterior_is_string_zero(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].hashAnt, "0")
-    
-    def test_is_bloque_genesis_hash_bloque_should_to_be_true_when_hash_bloque_is_equal_to_getHashByIndex(self):
-        test = Blockchain()
-        self.assertEqual(test.cadena[0].hashBloque, test.getHashByIndex(0))
-        
 if __name__ == '__main__':
     unittest.main()
